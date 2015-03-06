4064f5698db8c10abdbc5ae19fd45ee1b5eb4e87
Paula C Schuler <paulasatx@gmail.com>
2015-03-06 10:17:44 -0600

    Whackamole: Timer and Score working.

--- public/js/whack.js
+++ public/js/whack.js
@@ -7,39 +7,52 @@
 //Update the code to remove the mole when it is clicked.
 
 //Add score. Each time the mole is successfully clicked, increase the displayed score by 1.
-var score = 0;
-
+var timer = 15;
+interval = 1500;
+score = 0;
 
 var getRandomCell = function() {
 	var random = Math.floor(Math.random() * 9);
 	var cells = $(".col-md-3");
 	console.log(cells);
+	
 	$(cells[random]).addClass("mole");
+	
 	var timeoutId = setTimeout (function(){
 	$(cells[random]).removeClass("mole");
-	},1500);
+	},1200);
+	
 	var userTarget = cells[random];
 }
-getRandomCell();
 
-// var compareWhack = function(event){
-// 	console.log("listening working");
-// 	if($(e.target).hasClass('mole')) {
-//     score += 1;
-//     $("#score").html(score);
-// 	}
-// }
+//game goes for 15 seconds and regardless of user activity
+var overallTimer = setInterval (function(){	
+	if (timer > 0){
+		getRandomCell();
+
+		timer--;
+		$("#timer").html(timer);
+	} else {
+		clearInterval(overallTimer);
+		alert("Game Over");
+		}
+	}, interval);
 
 $(".col-md-3").click(function(){
-	console.log("listening working");
+	console.log("heard");
 	if($(".col-md-3").hasClass('mole')) {
-    score += 1;
-    $("#score").html(score);
+		$(".col-md-3").removeClass("mole");
+		getPoint();
+		score += 1;
+	$("#score").html(score);
 	}
 });
-
-
-
+		
+var getPoint = function(score){
+	console.log("in getPoint");
+	// score +=
+	// $("#score").html(score);
+}
 
 
 

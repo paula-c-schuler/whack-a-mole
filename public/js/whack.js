f54b986c68e9c0f71a425c889d3333892b230e99
Paula C Schuler <paulasatx@gmail.com>
2015-03-06 15:50:02 -0600

    Whackamole: Grid improved and centered. Next is improvement of appearance and tone.

--- public/js/whack.js
+++ public/js/whack.js
@@ -1,31 +1,23 @@
 //******** JAVASCRIPT FOR WHACKAMOLE GAME *********
 
-//Change the red background to use an image. 
-//This could be a mole, or any other image you'd like to use.
-
-
-//Update the code to remove the mole when it is clicked.
-
-//Add score. Each time the mole is successfully clicked, increase the displayed score by 1.
 var timer = 15;
 interval = 1500;
 score = 0;
 
+//generate and change the appearance of a random "mole"
 var getRandomCell = function() {
 	var random = Math.floor(Math.random() * 9);
-	var cells = $(".col-md-3");
+	var cells = $(".box");
 	console.log(cells);
 	
 	$(cells[random]).addClass("mole");
 	
 	var timeoutId = setTimeout (function(){
 	$(cells[random]).removeClass("mole");
-	},1200);
-	
-	var userTarget = cells[random];
+	},1000);
 }
 
-//game goes for 15 seconds and regardless of user activity
+//game goes for 15 seconds regardless of user activity
 var overallTimer = setInterval (function(){	
 	if (timer > 0){
 		getRandomCell();
@@ -38,21 +30,16 @@ var overallTimer = setInterval (function(){
 		}
 	}, interval);
 
-$(".col-md-3").click(function(){
-	console.log("heard");
-	if($(".col-md-3").hasClass('mole')) {
-		$(".col-md-3").removeClass("mole");
-		getPoint();
+//user click is verified valid or not, score is posted on display
+$(".box").click(function(){
+	if($(this).hasClass('mole')) {
+		$(".box").removeClass("mole");
 		score += 1;
-	$("#score").html(score);
-	}
+		$("#score").html(score);
+	} 
 });
 		
-var getPoint = function(score){
-	console.log("in getPoint");
-	// score +=
-	// $("#score").html(score);
-}
+
 
 
 

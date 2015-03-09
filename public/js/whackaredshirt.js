b0496d081a30a195a4ed1b17e8f1f6a95b0f6de6
Paula C Schuler <paulasatx@gmail.com>
2015-03-08 21:48:24 -0500

    Whackamole and Whackaredshirt, two games now. Need to verify git updating.

--- public/js/whackaredshirt.js
+++ public/js/whackaredshirt.js
@@ -1,9 +1,9 @@
 //******** JAVASCRIPT FOR WHACKAMOLE GAME *********
-//******** JAVASCRIPT FOR WHACKAMOLE GAME *********
 
 var timer = 15;
 interval = 1500;
 score = 0;
+ 
 
 //generate and change the appearance of a random "mole"
 var getRandomCell = function() {
@@ -15,7 +15,7 @@ var getRandomCell = function() {
 	
 	var timeoutId = setTimeout (function(){
 	$(cells[random]).removeClass("mole");
-	},1000);
+	},1300);
 }
 
 //game goes for 15 seconds regardless of user activity
@@ -27,16 +27,34 @@ var overallTimer = setInterval (function(){
 		$("#timer").html(timer);
 	} else {
 		clearInterval(overallTimer);
+		$("#start").removeClass("start");
 		alert("Game Over");
 		}
 	}, interval);
 
 //user click is verified valid or not, score is posted on display
 $(".box").click(function(){
+	//play tos_phaser audio*************** HOW DO I PLAY AUDIO? **********
 	if($(this).hasClass('mole')) {
 		$(".box").removeClass("mole");
 		score += 1;
 		$("#score").html(score);
-	} 
+	}
 });
+//button appears after game over and offers opportunity to play again, 
+//upon button click, it resets the score and calls game function
+var start = function(){
+$("#start").click(function() {
+	console.log("start heard");
+	score = 0;
+	$("#score").html(score);
+
+	getRandomCell();
+	});
+};
+
+		
+
+
+
 
